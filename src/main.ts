61b6fc7869698e8c7ea6aac3861f44c155ba8aef
José Eduardo <jose.es.oleg@gmail.com>
2024-07-26 10:28:27 -0300

    server port listening console

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
   const port = process.env.PORT || 5000
-  await app.listen(port);
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
